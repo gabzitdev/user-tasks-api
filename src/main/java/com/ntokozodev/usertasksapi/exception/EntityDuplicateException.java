bf1f21ac997d9d03527cbf25e95f58eeeb321d17
ntokozodev <ntokozodev@gmail.com>
2022-07-31 15:48:59 +0200

    Update exceptions

--- src/main/java/com/ntokozodev/usertasksapi/exception/EntityDuplicateException.java
+++ src/main/java/com/ntokozodev/usertasksapi/exception/EntityDuplicateException.java
@@ -1,17 +1,6 @@
 package com.ntokozodev.usertasksapi.exception;
 
 public class EntityDuplicateException extends Exception {
-    public EntityDuplicateException() {
-    }
-
-    public EntityDuplicateException(String message) {
-        super(message);
-    }
-
-    public EntityDuplicateException(Throwable cause) {
-        super(cause);
-    }
-
     public EntityDuplicateException(String message, Throwable cause) {
         super(message, cause);
     }
--- src/main/java/com/ntokozodev/usertasksapi/exception/ServiceException.java
+++ src/main/java/com/ntokozodev/usertasksapi/exception/ServiceException.java
@@ -1,17 +1,6 @@
 package com.ntokozodev.usertasksapi.exception;
 
 public class ServiceException extends Exception {
-    public ServiceException() {
-    }
-
-    public ServiceException(String message) {
-        super(message);
-    }
-
-    public ServiceException(Throwable cause) {
-        super(cause);
-    }
-
     public ServiceException(String message, Throwable cause) {
         super(message, cause);
     }
