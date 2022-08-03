1c78e96d9cf518f63892379dc8854d1df7ee630e
ntokozodev <ntokozodev@gmail.com>
2022-08-03 19:05:25 +0200

    Add more example insert scripts

--- src/main/resources/data-example.sql
+++ src/main/resources/data-example.sql
@@ -1,2 +1,27 @@
-INSERT INTO USERS values(1, 'Ntokozo', 'Dev', 'ntokozodev');
-INSERT INTO TASKS values(1, '2022-07-31 14:25:00', 'Description of task', 'My task', 1);
+INSERT INTO USERS
+values(1, 'Bob', 'Dev', 'bobs'),
+(2, 'Foo', 'Dev', 'foos');
+
+INSERT INTO TASKS
+values(1, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(2, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(3, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(4, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(5, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(6, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(7, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(8, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(9, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(10, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(11, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(12, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(13, '2022-07-31 14:25:00', 'Description of task', 'Bobs', 1),
+(14, '2022-07-31 14:25:00', 'Description of task', 'foos', 2),
+(15, '2022-07-31 14:25:00', 'Description of task', 'foos', 2),
+(16, '2022-07-31 14:25:00', 'Description of task', 'foos', 2),
+(17, '2022-07-31 14:25:00', 'Description of task', 'foos', 2),
+(18, '2022-07-31 14:25:00', 'Description of task', 'foos', 2),
+(19, '2022-07-31 14:25:00', 'Description of task', 'foos', 2),
+(20, '2022-07-31 14:25:00', 'Description of task', 'foos', 2),
+(21, '2022-07-31 14:25:00', 'Description of task', 'foos', 2),
+(22, '2022-07-31 14:25:00', 'Description of task', 'foos', 2);
